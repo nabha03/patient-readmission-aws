c20fa4dc6ea0a2d8cdd8a77a5bb91b7919ebd23c
nabha03 <148427357+nabha03@users.noreply.github.com>
2026-05-28 16:15:52 +0530

    Update trigger_glue.py

--- lambda/trigger_glue.py
+++ lambda/trigger_glue.py
@@ -1,1 +1,17 @@
+import boto3
 
+def lambda_handler(event, context):
+    glue = boto3.client('glue', region_name='ap-south-1')
+    
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    response = glue.start_job_run(
+        JobName='readmission-etl-job',
+        Arguments={
+            '--input_path': f's3://{bucket}/{key}',
+            '--output_path': 's3://readmission-processed-vaishali/output/'
+        }
+    )
+    print(f"Glue job started: {response['JobRunId']}")
+    return {'statusCode': 200, 'body': 'Glue job triggered'}
